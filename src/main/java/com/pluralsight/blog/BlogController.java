91207776e2e56e7b8bf60782f01b73dcfd357adc
jaineel.upadhyay <jaineel.upadhyay@accenture.com>
2022-01-10 15:35:24 -0500

    Display Static Data from the Post Repository

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,14 +1,27 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+    public BlogController(PostRepository pr){
+        postRepository = pr;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap mm){
-        mm.put("title", "Blog Post 1");
+        List<Post> posts = postRepository.getAllPosts();
+
+        mm.put("posts", posts);
         return "home";
     }
+
+    private PostRepository postRepository;
+
 }
